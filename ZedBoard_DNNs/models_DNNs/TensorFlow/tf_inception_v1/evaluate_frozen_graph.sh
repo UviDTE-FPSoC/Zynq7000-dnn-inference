de40a01df8be761ab3cb14af05e80edf3742b7ca
Arroas <xalorr@hotmail.com>
2020-06-21 14:24:25 +0200

    Added tf_inceptionv1 evaluation scripts

---
+++ ZedBoard_DNNs/models_DNNs/TensorFlow/tf_inception_v1/evaluate_frozen_graph.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+# Please set your imagenet validation dataset path here, 
+IMAGE_DIR=/media/arroas/HDD/MinhasCousas/EEI/Mestrado/2_Curso/TFM/Inference_Images/calibration_data/imagenet_images/
+IMAGE_LIST=/media/arroas/HDD/MinhasCousas/EEI/Mestrado/2_Curso/TFM/Inference_Images/calibration_data/val.txt
+
+# Please set your batch size settings here, #IMAGES = VAL_BATCHES * BATCH_SIZE
+# Commonly there are 5w image in total for imagenet validation dataset
+EVAL_BATCHES=50		#1000
+BATCH_SIZE=50
+
+python inception_v1_eval.py \
+  --input_frozen_graph ./float/inception_v1_inference.pb \
+  --input_node input \
+  --output_node InceptionV1/Logits/Predictions/Reshape_1 \
+  --eval_batches $EVAL_BATCHES \
+  --batch_size $BATCH_SIZE \
+  --eval_image_dir $IMAGE_DIR \
+  --eval_image_list $IMAGE_LIST \
+  --gpu 0
