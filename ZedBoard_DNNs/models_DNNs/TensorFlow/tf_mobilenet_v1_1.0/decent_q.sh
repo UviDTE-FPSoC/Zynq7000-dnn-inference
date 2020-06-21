10f84c05dff4a28b134de146edba567a650efb51
Arroas <xalorr@hotmail.com>
2020-06-21 13:16:43 +0200

    Added tf_mobilenetv1 application

---
+++ ZedBoard_DNNs/models_DNNs/TensorFlow/tf_mobilenet_v1_1.0/decent_q.sh
@@ -0,0 +1,9 @@
+decent_q quantize \
+  --input_frozen_graph ./float/mobilenet_v1_1.0_224.pb \
+  --input_nodes input \
+  --input_shapes ?,224,224,3 \
+  --output_nodes MobilenetV1/Predictions/Reshape_1 \
+  --input_fn mobilenet_v1_input_fn.calib_input \
+  --method 1 \
+  --gpu 0 \
+  --calib_iter 100 \
---
+++ ZedBoard_DNNs/models_DNNs/TensorFlow/tf_mobilenet_v1_1.0/mobilenet_v1_preprocessing.py
@@ -0,0 +1,26 @@
+import cv2
+from sklearn import preprocessing
+
+def mean_image_subtraction(image):
+  B, G, R = cv2.split(image)
+  image = cv2.merge([R, G, B])
+  return image
+
+def central_crop(image, scale=1.0):
+  center_y = image.shape[0] / 2
+  center_x = image.shape[1] / 2
+  width_scaled = image.shape[1]*scale
+  height_scaled = image.shape[0]*scale
+
+  left_x = center_x - width_scaled / 2
+  right_x = center_x + width_scaled / 2
+  top_y = center_y - height_scaled / 2
+  bottom_y = center_y + height_scaled / 2
+
+  return image[int(top_y):int(bottom_y), int(left_x):int(right_x)]
+
+def normalize(image):
+  image=image/255.0
+  image=image-0.5
+  image=image*2
+  return image
